ca3dca580d0a127ef1ed0650733e311578f7b47f
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-05-09 23:46:34 -0400

    rename and decouple cave gen from tilemap

--- scripts/cave_generation/cave_generator.gd
+++ scripts/cave_generation/cave_generator.gd
@@ -1,5 +1,5 @@
 class_name CaveGenerator
-extends TileMapLayer
+extends Node2D
 
 @export var cave_size := Vector2i(100, 100)
 @export var cave_room_count: int = 10
@@ -7,10 +7,14 @@ extends TileMapLayer
 @export var room_size_min: int = 50
 @export var room_size_max: int = 200
 
-var rooms: Array[Room]
-
 func _ready() -> void:
 	pass
+
+func generate_cave() -> PriorityList:
+	var rooms := PriorityList.new(func(a: float, b: float):
+		return a < b
+	)
+	return rooms
 
 func generate_room(pos: Vector2) -> void:
 	pass
--- scripts/util/priority_list.gd
+++ scripts/util/priority_list.gd
@@ -1,4 +1,4 @@
-class_name PriorityQueue
+class_name PriorityList
 extends RefCounted
 
 var _decider_func: Callable
