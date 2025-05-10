f4b6f5efd0b980be2ce3f6dddfe27106245083b7
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-05-09 23:51:00 -0400

    seed stuff

--- scripts/cave_generation/cave_generator.gd
+++ scripts/cave_generation/cave_generator.gd
@@ -8,13 +8,17 @@ extends Node2D
 @export var room_size_max: int = 200
 
 func _ready() -> void:
-	pass
+	randomize()
 
-func generate_cave() -> PriorityList:
+func generate_cave(gen_seed: int = -1) -> PriorityList:
+	var rng := RandomNumberGenerator.new()
+	rng.seed = gen_seed if gen_seed != -1 else randi_range(0, 2_147_483_646)
+
 	var rooms := PriorityList.new(func(a: float, b: float):
 		return a < b
 	)
 	return rooms
 
-func generate_room(pos: Vector2) -> void:
-	pass
+func generate_room(pos: Vector2) -> Room:
+	var room := Room.new(pos)
+	return room
--- scripts/cave_generation/room.gd
+++ scripts/cave_generation/room.gd
@@ -1,2 +1,8 @@
 class_name Room
 extends RefCounted
+
+var cells: Array[Vector2i]
+var center: Vector2i
+
+func _init(_center: Vector2i) -> void:
+	center = _center
