a840ab2364ca13b4491d5973c5688ab153511ece
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-05-09 23:40:41 -0400

    binary heap/priority list

--- scripts/util/priority_queue.gd
+++ scripts/util/priority_queue.gd
@@ -6,15 +6,48 @@ var _heap: Array[PriorityNode]
 
 func _init(_decider: Callable) -> void:
 	_decider_func = _decider
+	_heap = []
 
 func append(value, priority: float) -> void:
-	pass
+	_heap.append(PriorityNode.new(priority, value))
 
-func _left(i: int) -> void:
-	pass
+	if _heap.size() == 1: # if the element jsut added was the first element, no need to check
+		return
 
-func _right(i: int) -> void:
-	pass
+# cant use 'get' because its a native godot function :(
+func find(i: int):
+	return _heap[i].value
+
+func _cascade_up(i: int) -> void:
+	if i == 0:
+		return
+
+	var node = _heap[i]
+	var parent_idx := _parent(i)
+	var parent := _heap[parent_idx]
+	
+	if _decider_func.call(parent.priority, node.priority):
+		_swap(i, parent_idx)
+		_cascade_up(parent_idx)
+
+func _swap(i: int, j: int) -> void:
+	var n1 = _heap[i]
+	var n2 = _heap[j]
+
+	_heap[i].priority = n2.priority
+	_heap[i].value = n2.value
+	_heap[j].priority = n1.priority
+	_heap[i].value = n1.value
+
+func _parent(i: int) -> int:
+	@warning_ignore("INTEGER_DIVISION")
+	return floor((i - 1) / 2)
+
+func _left(i: int) -> int:
+	return (2 * i) + 1
+
+func _right(i: int) -> int:
+	return (2 * i) + 2
 
 class PriorityNode:
 	var priority: float
@@ -23,4 +56,3 @@ class PriorityNode:
 	func _init(_priority: float, _value) -> void:
 		priority = _priority
 		value = _value
-
