45f3ff5df42b2331538bdee60284be996cfa8c44
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-05-11 14:21:00 -0400

    non-working priority list shenanigans

--- scripts/cave_generation/cave_tilemap.gd
+++ scripts/cave_generation/cave_tilemap.gd
@@ -3,11 +3,14 @@ extends TileMapLayer
 
 @export var cave_tile_atlas_coords: Vector2i
 @export var source_id: int = 0
+@export var gem_count: int = 4
 
 @onready var cave_generator: CaveGenerator = $CaveGenerator
 
+var cave = null
+
 func _ready() -> void:
-	var cave := cave_generator.generate_cave()
+	cave = cave_generator.generate_cave()
 
 	for y in range(cave.size.y):
 		for x in range(cave.size.x):
@@ -17,3 +20,9 @@ func _ready() -> void:
 			
 			if is_wall:
 				set_cell(pos, source_id, cave_tile_atlas_coords)
+	
+	cave.room.walls.print()
+	for i in range(gem_count):
+		var j: int = (cave.room.walls.size() - 1) / (i + 1)
+		var wall: Vector2i = cave.room.walls.find(j)
+		set_cell(wall, source_id, Vector2i(3, 2) + Vector2i(i, 0))
--- scripts/cave_generation/room.gd
+++ scripts/cave_generation/room.gd
@@ -2,8 +2,10 @@ class_name Room
 extends RefCounted
 
 var cells: Array[Vector2i]
-var mean_y_level: float
+var walls: PriorityList
 
 func _init() -> void:
 	cells = []
-	mean_y_level = 0
+	walls = PriorityList.new(func(a, b):
+		return a < b
+	)
